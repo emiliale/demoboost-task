83f5740a999daed938788a475a44f017d45d2514
emiliale <emila.lebiedowska@gmail.com>
2021-11-26 23:19:00 +0000

    Update server.ts

--- server.ts
+++ server.ts
@@ -15,13 +15,6 @@ app.use(cors({
     origin: '*'
 }));
 
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-
 app.use(bodyParser.json());
 
 app.get('/webpages', async (req, res) => {
