9c74f8c056aefb23734a23aa0676267e71cd774b
emiliale <emila.lebiedowska@gmail.com>
2021-11-26 14:28:42 +0000

    Changes to deploy

--- server.ts
+++ server.ts
@@ -12,26 +12,26 @@ app.set("port", port);
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/webpages', async (req, res) => {
-    try{
-        // const results = await database.promise().query("SELECT * FROM webpages")
-        // res.status(200).send(results[0])
-    }catch(err){
-        console.log(err)
-    }
-})
+// app.get('/webpages', async (req, res) => {
+//     try{
+//         // const results = await database.promise().query("SELECT * FROM webpages")
+//         // res.status(200).send(results[0])
+//     }catch(err){
+//         console.log(err)
+//     }
+// })
 
-app.post('/webpages', (req, res) => {
-    const { name, address } = req.body;
-    if (name && address) {
-        try {
-            // database.promise().query(`INSERT INTO webpages VALUES('${name}', '${address}')`);
-            res.status(201).send({ msg: "Created webpage" });
-        }
-        catch (err) {
-            console.log(err)
-        }
-    }
-})
+// app.post('/webpages', (req, res) => {
+//     const { name, address } = req.body;
+//     if (name && address) {
+//         try {
+//             // database.promise().query(`INSERT INTO webpages VALUES('${name}', '${address}')`);
+//             res.status(201).send({ msg: "Created webpage" });
+//         }
+//         catch (err) {
+//             console.log(err)
+//         }
+//     }
+// })
 
 app.listen(port, () => console.log(`server started at port ${port}`))
