9eb3785cf39b0aae92952e995ff87da10c086c1f
emiliale <emila.lebiedowska@gmail.com>
2021-11-26 23:15:12 +0000

    Update server.ts

--- server.ts
+++ server.ts
@@ -11,7 +11,11 @@ app.set("port", port);
 
 const db = process.env.DB_ENVIROMENT ? database.production : database.development 
 
-app.use(cors());
+app.use(cors({
+    origin: '*'
+}));
+
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
