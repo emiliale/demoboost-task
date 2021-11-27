9042baedeee228d2f28849c5f01aeec07038f60c
emiliale <emila.lebiedowska@gmail.com>
2021-11-27 00:03:41 +0000

    Update database.ts

--- database.ts
+++ database.ts
@@ -14,7 +14,8 @@ export = {
             host: "d3y0lbg7abxmbuoi.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
             user: "rfzzbed04z80apx7",
             password: "qb89i6el8861z931",
-            database: "ao9xj0eyrku1qc0s"
+            database: "ao9xj0eyrku1qc0s",
+            port: 3306,
         })
     ,
 }
