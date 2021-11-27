e777b5e83e2f2d1eeb31456d2cfd7d684193d8da
emiliale <emila.lebiedowska@gmail.com>
2021-11-27 00:32:33 +0000

    Changed database connection

--- database.ts
+++ database.ts
@@ -1,12 +1,17 @@
 import mysql from 'mysql2'
 
-export const development =  mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "demoboosttask",
-})
-
-export const production =  mysql.createConnection(
-    "mysql://rfzzbed04z80apx7:qb89i6el8861z931@d3y0lbg7abxmbuoi.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/ao9xj0eyrku1qc0s"
+let database =
+    mysql.createConnection(
+        "mysql://rfzzbed04z80apx7:qb89i6el8861z931@d3y0lbg7abxmbuoi.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/ao9xj0eyrku1qc0s"
     )
+
+if(!process.env.DB_ENVIROMENT){
+    database = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "demoboosttask",
+    })
+}   
+
+export = database
--- server.ts
+++ server.ts
@@ -1,22 +1,20 @@
 import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
-import { production } from './database'
-import { development } from './database'
+import database from './database'
 
 const app = express()
 
 const port = process.env.PORT || 5000;
 app.set("port", port);
 
-const db = process.env.DB_ENVIROMENT ? production : development 
 
 app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/webpages', async (req, res) => {
     try{
-        const results = await db.promise().query("SELECT * FROM webpages")
+        const results = await database.promise().query("SELECT * FROM webpages")
         res.status(200).send(results[0])
     }catch(err){
         console.log(err)
@@ -27,7 +25,7 @@ app.post('/webpages', (req, res) => {
     const { name, address } = req.body;
     if (name && address) {
         try {
-            db.promise().query(`INSERT INTO webpages VALUES('${name}', '${address}')`);
+            database.promise().query(`INSERT INTO webpages VALUES('${name}', '${address}')`);
             res.status(201).send({ msg: "Created webpage" });
         }
         catch (err) {
